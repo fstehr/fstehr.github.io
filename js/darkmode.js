fbbf7abc0ebf81c7550b2856ae66f848e5255f61
Frauke Stehr <f.stehr@maastrichtuniversity.nl>
2022-04-18 19:10:37 +0200

    fixed darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -5,10 +5,12 @@ let darkmodetoggle = document.getElementById("DarkModeToggle");
 // on button click change color mode
 darkmodetoggle.addEventListener("click", function () {
     let current_mode = document.documentElement.getAttribute("color-mode");
-    if (current_mode === "dark"){
+    if (localStorage.lightMode === "dark"){
+        localStorage.lightMode = "light";
         document.documentElement.setAttribute("color-mode", "light");
     }
     else {
+        localStorage.lightMode = "dark";
         document.documentElement.setAttribute("color-mode", "dark");
     }
 
