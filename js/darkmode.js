372e69fd5adb74031c0869f8c18b6bc79806adee
Frauke Stehr <f.stehr@maastrichtuniversity.nl>
2022-04-19 08:33:01 +0200

    updated darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -6,27 +6,27 @@ let darkmodetoggle = document.getElementById("DarkModeToggle");
 document.addEventListener("DOMContentLoaded", function() {
   if (sessionStorage.lightMode === undefined) {
       sessionStorage.lightMode = "dark";
-      console.log("localstorage lightMode on load", sessionStorage.lightMode);
+      console.log("sessionStorage lightMode on load", sessionStorage.lightMode);
   }
   else {
       /* on load set color mode to the one stored in localstorage */
-      console.log("localstorage lightMode on load", sessionStorage.lightMode);
+      console.log("sessionStorage lightMode on load", sessionStorage.lightMode);
       document.documentElement.setAttribute("color-mode", sessionStorage.lightMode);
   }
 });
 
 // on button click change color mode
 darkmodetoggle.addEventListener("click", function () {
-    console.log("localstorage lightMode on click", sessionStorage.lightMode)
+    console.log("sessionStorage lightMode on click", sessionStorage.lightMode)
     if (sessionStorage.lightMode === "dark"){
         document.documentElement.setAttribute("color-mode", "light");
         sessionStorage.lightMode = "light";
-        console.log("localstorage lightMode after if", sessionStorage.lightMode)
+        console.log("sessionStorage lightMode after if", sessionStorage.lightMode)
     }
     else {
         document.documentElement.setAttribute("color-mode", "dark");
         sessionStorage.lightMode = "dark";
-        console.log("localstorage lightMode after else", sessionStorage.lightMode)
+        console.log("sessionStorage lightMode after else", sessionStorage.lightMode)
     }
 
 });
