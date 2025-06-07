33df2fda5764ca63d1cc6222748b3f116bfacd4d
Frauke Stehr <f.stehr@maastrichtuniversity.nl>
2025-06-07 21:51:20 +0200

    added new layout

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,17 +2,21 @@
 
 let darkmodetoggle = document.getElementById("DarkModeToggle");
 
-/* on page load define lightMode and apply it to css */
-document.addEventListener("DOMContentLoaded", function() {
-    /* initialize variable in sessionStorage */
-  if (sessionStorage.lightMode === undefined) {
-      sessionStorage.lightMode = "dark";
-  }
-  else {
-      /* on load set color mode to the one stored in sessionStorage */
-      document.documentElement.setAttribute("color-mode", sessionStorage.lightMode);
-  }
-});
+
+// Automatically Check user preference for color scheme on page load
+//  document.addEventListener('DOMContentLoaded', function () {
+    // Check the user's system color scheme preference
+//    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    // Set the `color-mode` attribute on the <html> element based on preference
+//    if (prefersDarkScheme) {
+//      document.documentElement.setAttribute('color-mode', 'dark');
+//    } else {
+//      document.documentElement.setAttribute('color-mode', 'light');
+//    }
+//  });
+
+
 
 
 /* on button click change color mode in css and save in sessionStorage */
